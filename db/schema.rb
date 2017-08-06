1f07bf02b81be7fb8c839623f8fa9b3706070238
Luke Fitzpatrick <lukejamesfitzpatrick@gmail.com>
2017-08-06 12:16:02 +1000

    Add names to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170725010304) do
+ActiveRecord::Schema.define(version: 20170806011153) do
 
-  create_table "pins", force: :true do |t|
+  create_table "pins", force: :cascade do |t|
     t.string   "description",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20170725010304) do
 
   add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
-  create_table "users", force: :true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170725010304) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
