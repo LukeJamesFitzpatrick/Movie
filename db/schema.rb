ad3690b7d13cecf7b6c9fd2b927bab4aa2dd8a53
Luke Fitzpatrick <lukejamesfitzpatrick@gmail.com>
2017-08-30 15:46:54 +1000

    Modified pins controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170829092822) do
+ActiveRecord::Schema.define(version: 20170830030544) do
 
   create_table "likes", force: :cascade do |t|
     t.integer  "pin_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170829092822) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "link"
   end
 
   add_index "pins", ["user_id"], name: "index_pins_on_user_id"
