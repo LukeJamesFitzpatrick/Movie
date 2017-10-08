b69f42e831bae639e432c40cf48474d97a381cfd
Luke Fitzpatrick <lukejamesfitzpatrick@gmail.com>
2017-10-08 15:20:38 +1100

    Added Press Kit link

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,15 @@ Rails.application.configure do
 
   #Thoughtbot says to add this line to fix the Paperclip bug.
   Paperclip.options[:command_path] = "/usr/local/bin/"
+
+  # Sets paperclip to upload images to Amazon S3
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
 end
