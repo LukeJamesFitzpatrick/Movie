8c2aa8120294208e7cf302893b8a796df001260e
Luke Fitzpatrick <lukejamesfitzpatrick@gmail.com>
2017-10-14 12:14:35 +1100

    modified config gibbon.rb file

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,3 +1,6 @@
 require 'gibbon'
 gibbon = Gibbon::Request.new(api_key: "7485132cf5e6eb8ed339838df2ceb78e-us16")
+Gibbon::Request.open_timeout = 15
+Gibbon::Request.symbolize_keys = true
+Gibbon::Request.debug = false
 gibbon.timeout = 10
