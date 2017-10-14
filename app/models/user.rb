9750be062a5d1ffa2aa0a24a94e95bbdddf83f3b
Luke Fitzpatrick <lukejamesfitzpatrick@gmail.com>
2017-10-14 16:35:35 +1100

    updated user.rb file for mailchimp

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,6 @@ class User < ActiveRecord::Base
   has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
   has_many :following, through: :following_relationships, source: :following
 
-  after_create :subscribe_user_to_mailing_list
-
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -19,11 +17,41 @@ class User < ActiveRecord::Base
 
 	validates :name, presence: true
 
-  private
-
-  def add_to_list
-    list_id = “4b2b17f02b” @gb = Gibbon::Request.new subscribe = @gb.lists(list_id).members.create(body: { email_address: self.email, status: “subscribed”, double_optin: false })
+  if ENV['GMAIL_DOMAIN'] == 'http://gammanu.org/'
+    after_create :send_alert_email
+    after_create :check_to_send_mailchimp_email
   end
+
+  def name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def send_alert_email
+    UserMailer.delay(queue: 'email', run_at: 5.minutes.from_now).new_user_alert_email(self)
+  end
+
+  def check_to_send_mailchimp_email
+    if check_mailchimp_list_for_user? && self.user_status != 'potential new member'
+      mailchimp_send_email_invite
+    end
+  end
+
+  def check_mailchimp_list_for_user?
+    list_id = "4b2b17f02b"
+    gb = Gibbon::API.new
+    a = gb.lists.member_info({:id => list_id, :emails => [{:email => self.email}]})
+    a["success_count"] == 0
+  end
+
+  def mailchimp_send_email_invite
+    UserMailer.delay(queue: 'email', run_at: 5.minutes.from_now).mailchimp_sign_up_user_email(self)
+  end
+
+  def send_mailchimp_email
+    mailchimp_send_email_invite
+    redirect_to root_path, notice: "Thanks for registering for the Gamma Nu's Alumni News Letter!"
+  end
+
 
   def follow(user_id)
       following_relationships.create(following_id: user_id)
